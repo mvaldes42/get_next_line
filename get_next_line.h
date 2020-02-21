b32a93ab25c7a35e4e88f881941d923627210c0f
Marie-lou VALDES <mvaldes@e2r6p8.42.fr>
2020-02-21 12:51:12 +0100

    nochanges

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 15:05:57 by mvaldes           #+#    #+#             */
-/*   Updated: 2020/02/21 11:50:14 by mvaldes          ###   ########.fr       */
+/*   Updated: 2020/02/21 12:02:45 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 8
+# define BUFF_SIZE 42
 
 # include <unistd.h>
 # include <stdlib.h>
